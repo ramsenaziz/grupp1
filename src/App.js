a435dec3490e143a4d18171b8a867dcc78699418
Ramsen Aziz <ramsen.aziz.privat@gmail.com>
2017-02-07 16:17:05 +0100

    Dice roll and card point calculator

--- src/App.js
+++ src/App.js
@@ -3,29 +3,66 @@ import './css/App.css';
 import Column from './Column.js';
 import Card from './Card.js';
 import Dice from './Dice.js';
+import ProgressBar from './ProgressBar.js';
+import ProgressBtn from './ProgressBtn.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      backlogCards: [{
-        key: 'US1',
-        title: 'US1',
-        money: 231,
-        analysis: 2,
-        development: 5,
-        testing: 3
-      }],
+      backlogCards: [],
       analysisCards: [],
       developmentCards: [],
       testingCards: [],
       doneCards: [],
-      unexpectedCards: []
+      unexpectedCards: [],
+      progress: 0,
+      diceValue: 0
     }
+
     this.handleCardClick = this.handleCardClick.bind(this);
   }
+
+  reducePoints(array, points, key, pos=0) {
+    var nextPos = pos +1;
+    var currentPoints = array[pos][key];
+    console.log(points);
+      while(currentPoints > 0 && points > 0) {
+       currentPoints --;
+       points --;
+      }
+      array[pos][key] = currentPoints;
+      this.setState({analysisCards: array});
+        if(points > 0 && array[nextPos]) {
+          this.reducePoints(array, points, key, nextPos)
+        }
+  }
+  
+  init() {
+    var backlog = this.state.backlogCards;
+    var analysis = this.cardGenerator(10);
+    var development = this.state.developmentCards;
+    var testing = this.state.testingCards;
+    var done = this.state.doneCards;
+    var unexpected = this.state.unexpectedCards;
+    this.setState({analysisCards: analysis}) 
+  }
+
+  nextDay() {
+    var number = this.state.progress + 20;
+    if(number > 100) {
+      number = 0;
+    }
+    this.init();
+    this.setState({progress: number});
+  }
+
+  rollDice() {
+    var stateValue = (this.random(6) + 1);
+    this.reducePoints(this.state.analysisCards, stateValue, 'analysis');
+  }
+
   handleCardClick(card) {
-    console.log(card);
     var targetArray = this.state.analysisCards.slice();
     targetArray.push(card);
 
@@ -76,7 +113,7 @@ class App extends Component {
         analysis: this.random(10) + 1,
         development: this.random(10) + 1,
         testing: this.random(10) + 1,
-        location: [locations[0]]
+        location: locations[1]
       });
     }
 
@@ -93,8 +130,11 @@ class App extends Component {
 
     return (
       <div className='container'>
+      <div className="row">
+      <ProgressBar bar={this.state.progress} />
+      </div>
         <div className='row'>
-          <Dice />
+          <Dice roll={this.rollDice.bind(this)}/>
         </div>
         <div className='row'>
           <Column key='b' title='Backlog' cards={this.createCards(backlog)} />
@@ -103,6 +143,9 @@ class App extends Component {
           <Column key='t' title='Testing' cards={this.createCards(testing)}/>
           <Column key='dn' title='Done' cards={this.createCards(done)}/>
           <Column key='un' title='Unexpected' cards={this.createCards(unexpected)}/>
+        </div>
+        <div className="row">
+        <ProgressBtn handleClick={this.nextDay.bind(this)} />
         </div>
       </div>
     )

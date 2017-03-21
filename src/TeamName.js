3318370eb585ffae5359f6820091e8c7d641d93c
Skruvkork <wille.nystrom@gmail.com>
2017-03-21 13:08:11 +0100

    Admin button added

--- src/TeamName.js
+++ src/TeamName.js
@@ -3,46 +3,48 @@ import './css/Retrospective.css';
 
 class TeamName extends Component {
 
-    render() {
-        var display = this.props.visible ? 'block' : 'none';
-        var style = { display: display };
-        let input;
-        return (
-            <div style={style} className='background'>
-                <div
-                    style={{
-                        background: 'black',
-                        color: 'white',
-                        paddingTop: '0px',
-                        paddingLeft: '40px',
-                        paddingBottom: '20px',
-                        border: '5px white solid',
-                    }}
-                    className='retrospective well well-lg'>
+	render() {
+		var display = this.props.visible ? 'block' : 'none';
+		var style = { display: display };
+		let input;
+		return (
+			<div style={style} className='background'>
+				<div
+					style={{
+						background: 'black',
+						color: 'white',
+						paddingTop: '0px',
+						paddingLeft: '40px',
+						paddingBottom: '20px',
+						border: '5px white solid',
+					}}
+					className='retrospective well well-lg'>
 
-                    <h2>Write your teamname</h2>
-                    <input
-                        style={{
-                            backgroundColor: 'black',
-                            marginTop:'10px',
-                            paddingLeft:'10px',
-                        }}
-                        ref={node => { input = node; }} />
+					<h2>Enter a teamname</h2>
+					<input
+						style={{
+							backgroundColor: 'black',
+							marginTop: '10px',
+							paddingLeft: '10px',
+						}}
+						ref={node => { input = node; }}
+					/>
 
-                    <button 
-                    style={{
-                        marginTop:'5px',
-                        marginLeft:'10px',
-                    }}
-                    className='btn btn-primary' onClick={() => {
-                        this.props.startgame(input.value);
-                    }}> New Game
-                    </button>
-                </div>
-            </div>
+					<button
+						style={{
+							marginTop: '5px',
+							marginLeft: '10px',
+						}}
+						className='btn btn-primary' onClick={() => {
+							this.props.startgame(input.value);
+						}}> New Game
+					</button>
+					<button className='btn btn-default'> <span className='glyphicon glyphicon-cog' aria-hidden='true'></span></button>
+				</div>
+			</div>
 
-        )
-    }
+		)
+	}
 }
 
 export default TeamName;

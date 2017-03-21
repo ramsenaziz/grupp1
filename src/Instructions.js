5dc90b41e51975047944bc7a8399e649b326ec12
Ramsen Aziz <ramsen.aziz.privat@gmail.com>
2017-03-21 15:44:54 +0100

    Changed styling to instructions

--- src/Instructions.js
+++ src/Instructions.js
@@ -19,11 +19,15 @@ class Instructions extends Component {
 	componentDidMount() {
 	    axios.get('http://localhost/grupp1/src/api/?/instructions').then((response) => {
 	        var instructions = response.data.map((obj, i) => {
-						return (<div key={obj.id}><h3>{obj.title}</h3>
-							<p>{obj.description.split('\n').map((item, key) => {
-								return <span key={key}>{item}<br /></span>
-							})}</p>
-							</div>);
+	            return (
+	            <div key={obj.id}><h3>{obj.title}</h3>
+	            	<div id="description">
+	            		<p>{obj.description.split('\n').map((item, key) => {
+	            			return <span key={key}>{item}<br /></span>
+	            		})}</p>
+	            	</div>
+	            </div>);
+
 	        });
 	        this.setState({ instructions: instructions});
 	    });
@@ -34,18 +38,20 @@ class Instructions extends Component {
   	var style = { display: display };
 	return (
 		<div>
-	  		<div className="glyphicon glyphicon-question-sign instructions"
+	  		<div className="glyphicon glyphicon-question-sign instructions" title="game-instructions"
 	  			onClick={this.displayInstructions.bind(this)}>
 
 	  		</div>
 
 	  		<div className="background" style={style} onClick={this.displayInstructions.bind(this)}>
 		  		<div className="text-container well" style={style}>
-		  		{this.state.instructions} Click on the screen to exit.
+		  		{this.state.instructions}
+			  		<div id="exit-text">
+			  			Click on the screen to go back to the game.
+			  		</div>
 	  			</div>
   			</div>
-
-  		</div>
+  		</div>    
   	)
   }
 }
